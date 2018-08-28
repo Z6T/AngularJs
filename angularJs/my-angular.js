df4331f936a2d2dc772d0e2e24e5794874b066b9
Z6T <1037457278@qq.com>
2018-08-28 08:56:52 +0800

    提交

---
+++ angularJs/my-angular.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+	var arrApp = Array.from(document.getElementsByTagName('*')).filter(ele=>ele.getAttribute('ng-app')!=undefined);
+	//处理ng-app
+	arrApp.forEach(app=>{
+		function findDerective(directive) {
+			let arr = [];
+			var arrApp = Array.from(app.getElementsByTagName('*')).forEach(ele=>{
+				let str = ele.getAttribute(directive);
+				if(str){
+					//如果键和值相等,就可以省略一个---ES6
+					models.push({name:str,ele});
+				}
+			})
+		}
+
+
+		let $scope = {};
+		//找到所有的ng-model
+		let models = findDerective('ng-model')
+		//数据===>HTML
+		function apply() {
+			for(let name in $scope){
+				models.forEach(json=>{
+					if(json.name==name){
+						json.ele.value = $scope[name];
+					}
+				})
+			}
+		}
+
+		//html ===>数据
+		models.forEach(json=>{
+			json.ele.oninput = function(){
+				$scope[json.model] = this.value;
+				apply();
+			}
+		})
+
+		//ng-bind
+
+		let binds = findDerective('ng-bind');
+		binds.forEach(json=>{
+			json.ele.innerHTML = json.name;
+		})
+
+	})
+
+
+
+}
